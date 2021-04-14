726e86890951b74107bcc35d5af28938e4008cfb
DC2305 <david_cvetkovic@sluz.ch>
2021-04-14 16:28:02 +0200

    Mit Formular angefangen

--- MeineTransportApp/Form1.cs
+++ MeineTransportApp/Form1.cs
@@ -16,5 +16,24 @@ namespace MeineTransportApp
         {
             InitializeComponent();
         }
+
+        private void Hilfe_Click(object sender, EventArgs e)
+        {
+            MessageBox.Show("Nachdem Sie eine Start- und Endstation eingegeben haben, " +
+                "können Sie auf 'Suchen' drücken, um die nächsten Verbindungen zu sehen oder auf 'Löschen'," +
+                "um die von Ihnen eingegebenen Stationen aus dem Textfeld zu löschen.\n" +
+                "Wenn Sie 'Abfahrtstafel' auswählen, zeigt es Ihnen alle Verbindungen, die von der eingegebenen " +
+                "Station weg gehen.", "Hilfe", MessageBoxButtons.OK, MessageBoxIcon.Information);
+        }
+
+        private void Suchen_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void Loeschen_Click(object sender, EventArgs e)
+        {
+
+        }
     }
 }
