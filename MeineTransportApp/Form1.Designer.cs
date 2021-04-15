470c7e3946bce61e91f73ef032ee13140f24a50d
DC2305 <david_cvetkovic@sluz.ch>
2021-04-15 16:29:59 +0200

    Verbindungen

--- MeineTransportApp/Form1.Designer.cs
+++ MeineTransportApp/Form1.Designer.cs
@@ -29,12 +29,17 @@
         private void InitializeComponent()
         {
             this.label1 = new System.Windows.Forms.Label();
-            this.textBox1 = new System.Windows.Forms.TextBox();
             this.label2 = new System.Windows.Forms.Label();
-            this.textBox2 = new System.Windows.Forms.TextBox();
             this.button1 = new System.Windows.Forms.Button();
             this.button3 = new System.Windows.Forms.Button();
             this.button2 = new System.Windows.Forms.Button();
+            this.comboBox1 = new System.Windows.Forms.ComboBox();
+            this.comboBox2 = new System.Windows.Forms.ComboBox();
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.Column1 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Column2 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Column3 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
             this.SuspendLayout();
             // 
             // label1
@@ -46,13 +51,6 @@
             this.label1.TabIndex = 0;
             this.label1.Text = "Startstation:";
             // 
-            // textBox1
-            // 
-            this.textBox1.Location = new System.Drawing.Point(96, 25);
-            this.textBox1.Name = "textBox1";
-            this.textBox1.Size = new System.Drawing.Size(239, 22);
-            this.textBox1.TabIndex = 1;
-            // 
             // label2
             // 
             this.label2.AutoSize = true;
@@ -61,13 +59,6 @@
             this.label2.Size = new System.Drawing.Size(79, 17);
             this.label2.TabIndex = 2;
             this.label2.Text = "Endstation:";
-            // 
-            // textBox2
-            // 
-            this.textBox2.Location = new System.Drawing.Point(426, 25);
-            this.textBox2.Name = "textBox2";
-            this.textBox2.Size = new System.Drawing.Size(239, 22);
-            this.textBox2.TabIndex = 3;
             // 
             // button1
             // 
@@ -99,20 +90,78 @@
             this.button2.UseVisualStyleBackColor = true;
             this.button2.Click += new System.EventHandler(this.Loeschen_Click);
             // 
+            // comboBox1
+            // 
+            this.comboBox1.FormattingEnabled = true;
+            this.comboBox1.Location = new System.Drawing.Point(96, 25);
+            this.comboBox1.Name = "comboBox1";
+            this.comboBox1.Size = new System.Drawing.Size(239, 24);
+            this.comboBox1.TabIndex = 9;
+            this.comboBox1.TextUpdate += new System.EventHandler(this.Startstation_TextUpdate);
+            // 
+            // comboBox2
+            // 
+            this.comboBox2.FormattingEnabled = true;
+            this.comboBox2.Location = new System.Drawing.Point(426, 25);
+            this.comboBox2.Name = "comboBox2";
+            this.comboBox2.Size = new System.Drawing.Size(239, 24);
+            this.comboBox2.TabIndex = 10;
+            this.comboBox2.TextUpdate += new System.EventHandler(this.Endstation_TextUpdate);
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Columns.AddRange(new System.Windows.Forms.DataGridViewColumn[] {
+            this.Column1,
+            this.Column2,
+            this.Column3});
+            this.dataGridView1.Location = new System.Drawing.Point(15, 113);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.RowHeadersWidth = 51;
+            this.dataGridView1.RowTemplate.Height = 24;
+            this.dataGridView1.Size = new System.Drawing.Size(574, 255);
+            this.dataGridView1.TabIndex = 12;
+            // 
+            // Column1
+            // 
+            this.Column1.HeaderText = "Datum";
+            this.Column1.MinimumWidth = 6;
+            this.Column1.Name = "Column1";
+            this.Column1.ReadOnly = true;
+            this.Column1.Width = 125;
+            // 
+            // Column2
+            // 
+            this.Column2.HeaderText = "Abfahrt";
+            this.Column2.MinimumWidth = 6;
+            this.Column2.Name = "Column2";
+            this.Column2.ReadOnly = true;
+            this.Column2.Width = 125;
+            // 
+            // Column3
+            // 
+            this.Column3.HeaderText = "Ankunft";
+            this.Column3.MinimumWidth = 6;
+            this.Column3.Name = "Column3";
+            this.Column3.ReadOnly = true;
+            this.Column3.Width = 125;
+            // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.dataGridView1);
+            this.Controls.Add(this.comboBox2);
+            this.Controls.Add(this.comboBox1);
             this.Controls.Add(this.button2);
             this.Controls.Add(this.button3);
             this.Controls.Add(this.button1);
-            this.Controls.Add(this.textBox2);
             this.Controls.Add(this.label2);
-            this.Controls.Add(this.textBox1);
             this.Controls.Add(this.label1);
             this.Name = "Form1";
             this.Text = "Transport";
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
             this.ResumeLayout(false);
             this.PerformLayout();
 
@@ -121,12 +170,16 @@
         #endregion
 
         private System.Windows.Forms.Label label1;
-        private System.Windows.Forms.TextBox textBox1;
         private System.Windows.Forms.Label label2;
-        private System.Windows.Forms.TextBox textBox2;
         private System.Windows.Forms.Button button1;
         private System.Windows.Forms.Button button3;
         private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.ComboBox comboBox1;
+        private System.Windows.Forms.ComboBox comboBox2;
+        private System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Column1;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Column2;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Column3;
     }
 }
 
--- MeineTransportApp/Form1.cs
+++ MeineTransportApp/Form1.cs
@@ -7,6 +7,8 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using SwissTransport.Core;
+using SwissTransport.Models;
 
 namespace MeineTransportApp
 {
@@ -28,12 +30,93 @@ namespace MeineTransportApp
 
         private void Suchen_Click(object sender, EventArgs e)
         {
+            ITransport transport = new Transport();     //Transport API wird verwendet
+            SwissTransport.Models.Connection temp2 = new SwissTransport.Models.Connection();
 
+            if (comboBox1.Text == "" && comboBox2.Text == "")
+            {
+                MessageBox.Show("Geben Sie noch die Start- und Endstation ein!");
+                return;
+            }
+
+            if (comboBox1.Text == "")
+            {
+                MessageBox.Show("Geben Sie noch die Startstation ein!");
+                return;
+            }
+
+            if (comboBox2.Text == "")
+            {
+                MessageBox.Show("Geben Sie noch die Endstation ein!");
+                return;
+            }
+
+            if (comboBox1.Text == comboBox2.Text)
+            {
+                MessageBox.Show("Nicht gleiche Stationsnamen eingeben!");
+                return;
+            }
+
+            dataGridView1.Rows.Clear();
+            foreach (SwissTransport.Models.Connection temp in transport.GetConnections(comboBox1.Text, comboBox2.Text).ConnectionList)
+            {
+                dataGridView1.Rows.Add(Verbindung(temp));
+            }
+        }
+
+        private string[] Verbindung(SwissTransport.Models.Connection temp)
+        {
+            string[] daten =
+            {
+                    temp.From.Departure.ToString().Substring(0, 10),     //Datum
+                    temp.From.Departure.ToString().Substring(11, 5),     //Zeit
+                    temp.To.Arrival.ToString().Substring(11, 5),         //Zeit
+                    temp.From.Platform
+            };
+            return daten;
         }
 
         private void Loeschen_Click(object sender, EventArgs e)
         {
+            comboBox1.Items.Clear();        //entfernt die vorgeschlagenen Stationen
+            comboBox1.ResetText();          //entfernt den eingegebenen Text
+            comboBox2.Items.Clear();
+            comboBox2.ResetText();
+            dataGridView1.Rows.Clear();     //entfernt die Verbindungen
+        }
 
+        private void VorschlagLeer(ComboBox transport)
+        {
+            transport.Items.Clear();
+            transport.SelectionStart = transport.Text.Length;
+            transport.SelectionLength = 0;
+        }
+
+        private void VorschlagVoll(ComboBox transport)
+        {
+            try
+            {
+                ITransport temp = new Transport();
+                transport.DroppedDown = true;
+                foreach (SwissTransport.Models.Station station in temp.GetStations(transport.Text).StationList)
+                {
+                    if (station.Name != null)
+                        transport.Items.Add(station.Name);
+                }
+            }
+            catch { }
+        }
+
+        private void Startstation_TextUpdate(object sender, EventArgs e)
+        {
+            VorschlagLeer(comboBox1);
+            VorschlagVoll(comboBox1);
+        }
+
+        private void Endstation_TextUpdate(object sender, EventArgs e)
+        {
+            VorschlagLeer(comboBox2);
+            VorschlagVoll(comboBox2);
         }
     }
 }
